2933912c70f61088570ba338f2c3fc7fd0aa60c5
Dhanesh Saini <branding.sanity@gmail.com>
2023-03-09 07:13:27 +0530

    1

--- site.js
+++ site.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')));
+const express = require('express')
+const path = require('path')
+const app = express()
+// using build folder
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
 app.listen(9000, function () {
-  console.log('http://localhost:9000/');
+  console.log('http://localhost:9000/')
 })
