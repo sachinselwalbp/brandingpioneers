6a03a04eee6e4f516218e49599cdb2411f7d4358
Dhanesh Saini <sainidhanesh58@gmail.com>
2023-03-31 21:13:38 +0530

    update
    favicon
    blog
    scripts

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,12 +1,23 @@
 import Helmet from 'react-helmet'
-import BlogCards from '../../components/blogCards'
 import Hero from '../../components/hero'
+import "./style.scss"
 
 export default function Blog() {
   return (
     <>
       <Helmet>
         <title>Blog</title>
+        {/* <link rel="stylesheet" href="https://cdn.bloghunch.com/assets/embed/app.css" /> */}
+        <script>
+          var bh_limit = 3;
+          var bh_tag = "Blog-  Healthcare digital marketing Agency";
+          var bh_author = "Branding Pioneers";
+          var bh_subdomain = "branding-pioneers.mybloghunch.com";
+          var bh_embed_title = "Latest Blogs";
+          var bh_embed_description = "Looking for a healthcare digital marketing agency to help you reach your target audience and grow your business? Our blog is your go-to source for expert insights and advice on all things related to healthcare digital marketing.";
+          var bh_id = "MjQ2MQ.PV_CVSuS3GMSQk8SmDKPurU_WGza_FW8jFkd0YILxgdiCRDqHe6LArqaY_m3";
+        </script>
+        <script src="https://cdn.bloghunch.com/assets/embed/app.js" defer></script>
       </Helmet>
       <div className="blog-container">
         <Hero title="Get updated with our internal affairs" />
@@ -17,23 +28,11 @@ export default function Blog() {
 }
 
 const BlogSection = () => {
-  const blogPosts = [
-    {
-      title: "First test Post",
-      slug: "/first-test-post",
-      date: "22 Jan 2023",
-      imgURL: "https://images.unsplash.com/photo-1677187315979-4a4be967000e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      alt: "First Post",
-      category: "Test"
-    },
-  ]
   return (
     <>
-      <section className='section-global bg-shade-blue'>
-        <div className="container">
-          <div className="row">
-            <BlogCards data={blogPosts} />
-          </div>
+      <section className='section-global'>
+        <div className="row blog_Section">
+          <div id='bh-app'></div>
         </div>
       </section>
     </>
