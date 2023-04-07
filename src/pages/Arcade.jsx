65f3378a117c3a5bab6b1d9701d937dee4cbe26a
Brandingpioneers <130143305+Brandingpioneers@users.noreply.github.com>
2023-04-07 15:55:47 +0530

    Update Arcade.jsx

--- src/pages/Arcade.jsx
+++ src/pages/Arcade.jsx
@@ -4,8 +4,9 @@ export default function Arcade() {
   return (
   <>
    <Helmet>
+    <title>Arcade</title>
+    </Helmet>
     <iframe class="airtable-embed" src="https://airtable.com/embed/shrBzcnsHAqgNqpgR?backgroundColor=yellow&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
-    </Helmet> 
     <>
   )
 }
