4f3342a9e542ac64f876773f8ae6666b51445d5b
Brandingpioneers <130143305+Brandingpioneers@users.noreply.github.com>
2023-04-07 15:51:39 +0530

    Update Arcade.jsx

--- src/pages/Arcade.jsx
+++ src/pages/Arcade.jsx
@@ -1,1 +1,11 @@
+import Helmet from "react-helmet"
 
+export default function Arcade() {
+  return (
+  <>
+   <Helmet>
+    <iframe class="airtable-embed" src="https://airtable.com/embed/shrBzcnsHAqgNqpgR?backgroundColor=yellow&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    </Helmet> 
+    <>
+  )
+}
